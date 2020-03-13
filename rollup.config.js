91b1a987511aea1263a34d099697de87b73dbde4
patomation <patomation@gmail.com>
2020-03-13 16:39:27 +0700

    improvement: creates cjs esm and iife modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,24 @@
 import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
 
 const input = 'src/index.ts'
 
 export default {
   input: input,
-  output: {
-    dir: 'lib',
-    format: 'cjs'
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'esm'
+    },
+    {
+      file: pkg.browser,
+      format: 'iife'
+    }
+  ],
   plugins: [
     typescript({
       tsconfig: 'tsconfig.json',
